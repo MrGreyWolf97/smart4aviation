408c40e4693917f238d0fe4d08e4df7be76df19a
cestaro <enrico.cestaro@targatelematics.com>
2023-09-18 20:03:19 +0200

    First commit

---
+++ src/test/kotlin/com/example/smart4aviation/Smart4aviationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.smart4aviation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Smart4aviationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
